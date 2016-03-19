c4ff6804a7d16618876a48bfab6fa4a7762e104f
lclevy <lclevy@free.fr>
2016-03-19 19:35:23 +0100

    80d values

--- cr2_database.sh
+++ cr2_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo -n "modelId, modelName, jpeg_bits, jpeg_wide, jpeg_high, jpeg_comp, slices[0], slices[1], slices[2], jpeg_hsf, jpeg_vsf, sensorLeftBorder, sensorTopBorder, sensorBottomBorder, sensorRightBorder,"
+echo -n "modelId, modelName, jpeg_bits, jpeg_wide, jpeg_high, jpeg_comp, jpeg_hsf, jpeg_vsf, slices[0], slices[1], slices[2], sensorWidth, sensorHeight, sensorLeftBorder, sensorTopBorder, sensorBottomBorder, sensorRightBorder,"
 echo "blackMaskLeftBorder,blackMasktopBorder,blackMaskBottomBorder,blackMaskRightBorder, imageWidth, imageHeight, vShift"
 for pic in `cat samples_list_rggb.txt samples_list_yuv.txt`; do
   src/craw2tool -m $pic
